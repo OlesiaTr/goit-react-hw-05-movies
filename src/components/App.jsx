1f5630b3f37bb072c6c2355ecdb895018711db30
OlesiaTr <104642736+OlesiaTr@users.noreply.github.com>
2023-01-22 13:41:15 +0200

    feat: add routes, pages, components

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,31 @@
+// Core
+import { lazy } from 'react';
+
+// Utils
+import { Route, Routes } from 'react-router-dom';
+
+// Components
+import { SharedLayout } from './SharedLayout';
+const Cast = lazy(() => import('./Cast/Cast'));
+const Reviews = lazy(() => import('./Reviews/Reviews'));
+
+// Pages
+const Home = lazy(() => import('../pages/Home'));
+const Movies = lazy(() => import('../pages/Movies'));
+const MovieDetails = lazy(() => import('../pages/MovieDetails'));
+
 export const App = () => {
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
+    <Routes>
+      <Route path="/" element={<SharedLayout />}>
+        <Route index element={<Home />} />
+        <Route path="movies" element={<Movies />} />
+        <Route path="movies/:movieId" element={<MovieDetails />}>
+          <Route path="cast" element={<Cast />} />
+          <Route path="reviews" element={<Reviews />} />
+        </Route>
+        <Route path="*" element={<Home />} />
+      </Route>
+    </Routes>
   );
 };
--- src/index.js
+++ src/index.js
@@ -1,10 +1,17 @@
+// Core
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+
+// Utils
+import { BrowserRouter } from 'react-router-dom';
+
+// Components
 import { App } from 'components/App';
-import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter basename="/goit-react-hw-05-movies">
+      <App />
+    </BrowserRouter>
   </React.StrictMode>
 );
